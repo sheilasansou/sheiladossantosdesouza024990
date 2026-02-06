fd730c403f34b632b59b9f36dc315e1fb9a7139b
43303 <43303@tjmt.jus.br>
2026-02-05 20:19:13 -0400

    feat(setup): estruturacao das 5 telas conforme endpoints da api

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { PetListComponent } from './pages/pet-list/pet-list.component';
+import { PetDetailComponent } from './pages/pet-detail/pet-detail.component';
+import { PetFormComponent } from './pages/pet-form/pet-form.component';
+import { TutorFormComponent } from './pages/tutor-form/tutor-form.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PetListComponent }, // Tela Inicial agora é a lista
+  { path: 'pet/novo', component: PetFormComponent },
+  { path: 'pet/editar/:id', component: PetFormComponent },
+  { path: 'pet/detalhes/:id', component: PetDetailComponent },
+  { path: 'tutor/novo', component: TutorFormComponent },
+  { path: 'tutor/editar/:id', component: TutorFormComponent },
+  { path: 'login', component: LoginComponent },
+];
---
+++ src/app/pages/tutor-form/tutor-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutor-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './tutor-form.component.html',
+  styleUrl: './tutor-form.component.css'
+})
+export class TutorFormComponent {
+
+}
